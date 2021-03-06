87372a291a6b5d1729b77f77e4c37517b0f1b091
thesmith4734 <thehellhound4734@aim.com>
2021-03-06 00:31:12 -0800

    implement fire command

--- cogs/general.py
+++ cogs/general.py
@@ -1,4 +1,4 @@
-import os, discord
+import os, discord, random
 from discord.ext import commands
 
 class general(commands.Cog, name="general"):
@@ -7,9 +7,21 @@ class general(commands.Cog, name="general"):
 
     @commands.command(name="fire")
     async def shoot(self, ctx):
-        await ctx.send("Pew")
- 
- #       await ctx.send("List of members: {}".format(ctx.voice_client.members))
+        # Check if user is in a voice channel
+        if ctx.author.voice is None:
+            await ctx.send("You have to be in a channel to use this command.")
 
+        # Pick a random user in the voice channel
+        voice_channel = ctx.author.voice.channel
+        shot_user = random.choice(voice_channel.members)
+
+        # Prioritize nickname over username
+        if shot_user.nick is None:
+            shot_user.nick = shot_user.name
+
+        # Remove User from voice channel and annonce them dead in text chat
+        await shot_user.move_to(None)
+        await ctx.send("Bang! {} was shot dead.".format(shot_user.nick))
+ 
 def setup(bot):
     bot.add_cog(general(bot))
